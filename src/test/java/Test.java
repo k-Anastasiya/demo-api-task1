958e791858ac83e1c5ede06fb3eae4fa0a551c6b
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-07-14 14:47:11 +0300

    add class Test

---
+++ src/test/java/Test.java
@@ -0,0 +1,36 @@
+import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+
+        try {
+            CloseableHttpClient  client= HttpClients.createDefault();
+            CloseableHttpResponse response= client.execute( new HttpGet("https://www.onliner.by/"));
+            HttpEntity entity = response.getEntity();
+            if (entity != null){
+                String data = IOUtils.toString(entity.getContent());
+                System.out.println("data" + data);
+            }
+            System.out.println();
+            for(Header header : response.getAllHeaders()) {
+                System.out.println(header.getName() + ":" + header.getValue());
+            }
+            System.out.println();
+            System.out.println("ProtocolVersion:" + response.getProtocolVersion());
+            System.out.println("Status Code"+ response.getStatusLine().getStatusCode());
+            System.out.println("reason phrase:" + response.getStatusLine().getReasonPhrase());
+            System.out.println("Status.line" + response.getStatusLine().toString());
+        } catch (Throwable caurse) {
+           caurse.printStackTrace();
+        }
+    }
+}
